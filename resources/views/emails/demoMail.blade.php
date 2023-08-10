7ab8b49834b68296fb2c3ea7b367362d570ae95c
sumithindika <tsumith44@gmail.com>
2023-08-10 23:48:02 +0530

    Added email send project

---
+++ resources/views/emails/demoMail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My test mail .com</title>
+</head>
+<body>
+    <h1>{{ $mailData['title'] }}</h1>
+    <p>{{ $mailData['body'] }}</p>
+
+    <p>This is test mail send it.</p>
+
+    <p>Thank you</p>
+</body>
+</html>
